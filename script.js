bbcba5c78f537b1f7d3982d34d0ae7c079af19b6
VICTOR DAVI <113daviiicamach@gmail.com>
2023-07-31 14:01:45 -0300

    add JS
    simple test DarkMode

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 
 
-var numero = 5; 
+// var numero = 5; 
 
-var titulo = document.getElementById('titulo');
+// var titulo = document.getElementById('titulo');
 
-for(var contador = 0; contador < numero; contador++){
+// for(var contador = 0; contador < numero; contador++){
   
 
-   titulo.style.color = 'red';
-}
+//    titulo.style.color = 'red';
+// }
     
+var buttonMode = document.querySelector('input#modeButton')
+var on = 'Dark Mode'
+var off = 'Clear Mode'
+buttonMode.addEventListener('click', ()=>{
+
+   if(buttonMode.value == on){
+      buttonMode.value = off
+
+      document.documentElement.style.setProperty("--color-principal", "grey")
+      document.documentElement.style.setProperty("--color-secundaria", "red")
+      document.documentElement.style.setProperty("--color-complementar", "white")
+   }
+   else{
+
+      buttonMode.value = on
+   
+      document.documentElement.style.setProperty("--color-principal", "blue")
+      document.documentElement.style.setProperty("--color-secundaria", "red")
+      document.documentElement.style.setProperty("--color-complementar", "white")
+   }
+
+})
+
